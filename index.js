35e5e3a10bf41afc6eb588be420a7cc947d7931c
Sakib-01 <syedsakibulhaque2@gmail.com>
2024-10-10 08:55:04 +0600

    load top news first

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const loadCategory = async () => {
     "https://newsdata.io/api/1/sources?country=bd&apikey=pub_55664120963394010ff7d91bc73d25d8e3cde"
   );
   const data = await res.json();
-  navbar(data.results[0].category);
+  navbar(data.results[5].category);
 };
 
 function navbar(categories) {
@@ -129,10 +129,108 @@ const loadNewsbyCategory = async (id) => {
   const data = await res.json();
   content(data.results);
 };
+const loadTopNews = async () => {
+  const res = await fetch(
+    `https://newsdata.io/api/1/latest?country=bd&category=top&apikey=pub_55664120963394010ff7d91bc73d25d8e3cde`
+  );
+  const data = await res.json();
+  topContent(data.results);
+};
+loadTopNews();
 
+// load top content
+
+const topNewsContainer = document.getElementById("news-item");
+function topContent(items) {
+  const modalContainer = document.getElementById("modal");
+  console.log(items);
+
+  // Clear previous news items
+  topNewsContainer.innerHTML = "";
+
+  items.forEach((item) => {
+    const newsItem = document.createElement("div");
+    console.log(item);
+    newsItem.innerHTML = `
+        <div class="w-full mx-auto  gap-6 p-6 bg-white sm:grid-cols-1 sm:w-full mb-6">
+          <div class="w-full ">
+            <div class="sm:flex sm:items-start">
+              <div class="sm:w-1/3">
+                <img
+                  src="${item.image_url}"
+                  alt="${item.title}"
+                  class="w-full h-auto rounded-lg object-cover"
+                />
+              </div>
+              <div class="sm:w-2/3 sm:ml-6 mt-4 sm:mt-0">
+                <h2 class="text-xl font-bold text-gray-800 hover:text-blue-800 cursor-pointer">
+                  ${item.title}
+                </h2>
+                <p class="mt-2 text-gray-600">${item.description}</p>
+                <p class="mt-4 text-sm text-gray-500">Published Date: ${item.pubDate}</p>
+                <p class="mt-4 text-sm text-blue-500">Source: ${item.source_name}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      `;
+    topNewsContainer.prepend(newsItem);
+
+    // Add click event to the title to show modal with content details
+    const titleElement = newsItem.querySelector("h2");
+    titleElement.addEventListener("click", () => {
+      modalContainer.innerHTML = `
+          <dialog id="my_modal_3" class="modal">
+            <div class="modal-box">
+              <div class="max-w-sm mx-auto bg-white rounded-lg shadow-md">
+                <img class="w-full h-56 object-cover" src="${item.image_url}" alt="Pet Image">
+                <div class="p-6">
+                  <h2 class="text-2xl font-semibold mb-2">${item.title}</h2>
+                  <div class="grid grid-cols-1 gap-4 text-sm text-gray-600">
+                    <div class="flex items-center space-x-2">
+                      <span>Description: ${item.description}</span>
+                    </div>
+                    <div class="flex items-center space-x-2">
+                      <span>Video: ${item.video_url}</span>
+                    </div>
+                    <div class="flex items-center space-x-2">
+                      <span>Category: ${item.category}</span>
+                    </div>
+                    <div class="flex items-center space-x-2 flex-wrap">
+                      <span>Source: ${item.source_name} - <a href="${item.link}" target="_blank">${item.link}</a></span>
+                    </div>
+                    <div class="flex items-center space-x-2">
+                      <span>Published Date: ${item.pubDate}</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="modal-action">
+                  <form method="dialog">
+                    <button class="btn btn-outline text-cyan-600 hover:bg-[rgba(14,122,129,0.5)] hover:text-black px-4 py-2 rounded-lg font-semibold">
+                      Cancel
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </dialog>
+        `;
+
+      const myModal = document.getElementById("my_modal_3");
+      if (myModal) {
+        myModal.showModal();
+      }
+    });
+  });
+}
+
+// load content by Category
 function content(items) {
   const newsContainer = document.getElementById("news-item");
   const modalContainer = document.getElementById("modal");
+  // topNewsContainer.style.display = "none";
+  console.log(items);
 
   // Clear previous news items
   newsContainer.innerHTML = "";
